b1cda7382acf06bdbfe87d2878d2177a9b8c25cc
Bennett <bjupfield@gmail.com>
2021-11-01 14:49:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+function wrapAdjective(visualflare = "*"){
+    return function notsaturdayFun(data = "special"){
+        return `You are ${visualflare}${data}${visualflare}!`
+    }
+}
+function saturdayFun(date = "roller-skate"){
+    return `This Saturday, I want to ${date}!`;
+}
+function mondayWork(date = false){
+    if(date === false){
+        return "This Monday, I will go to the office."
+    }
+    return `This Monday, I will ${date}.`
+}
